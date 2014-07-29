939b21d026c8b0c4f25322affbedd258eaa3a08b
benticsay <benticsay@gmail.com>
2014-07-29 09:31:11 -0400

    scores index

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,9 @@ Rails.application.routes.draw do
 
   get 'games/level4'
   
-  get 'games/high_scores'
-  
   get 'games/win_screen'
 
+  get 'scores/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
