6e2c4e796a9e0c2ba3ad1e6e058e6c69efb09d77
keyanbagheri <keyanbagheri@gmail.com>
2014-08-02 16:42:27 -0400

    timer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,15 +6,20 @@ class GamesController < ApplicationController
   end
 
   def level2
+    @level_one_time = params[:id]
   end
 
   def level3
+    @level_two_time = params[:id]
   end
 
   def level4
+    @level_three_time = params[:id]
   end
 
   def win_screen
+    @level_four_time = params[:id]
+    @total_time = @level_one_time + @level_two_time + @level_three_time + @level_four_time
   end
   
 end
